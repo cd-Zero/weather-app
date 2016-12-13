641f6f3947aae1a4c344f4dbd2062fd66bf40837
Michael Aduayi <Mike.Aduayi@gmail.com>
2016-12-13 14:55:55 -0500

    slight changes to file

--- js/main.js
+++ js/main.js
@@ -1,14 +1,42 @@
+
+var ourRequestLocation  = new XMLHttpRequest();
+
+
+
+ourRequestLocation.open("GET",'http://api.wunderground.com/api/62f3857a113070d5/geolookup/q/autoip.json',true);
+
+
+ourRequestLocation.onload = function location(){
+var locationData = JSON.parse(ourRequestLocation.responseText);
+    //test
+
+    renderHead(locationData);
+}
+
+ourRequestLocation.send();
+
+  function renderHead(data){
+    var rawTemplate2 = document.getElementById('headingTemplate').innerHTML;
+    var compiledTemplate = Handlebars.compile(rawTemplate2);
+    var ourGeneratedHTML  = compiledTemplate(data);
+    var weatherContainer = document.querySelector("#heading");
+    weatherContainer.innerHTML = ourGeneratedHTML;
+  }
+
+
+
 var ourRequest = new XMLHttpRequest();
 
 
 ourRequest.open('GET','http://api.wunderground.com/api/62f3857a113070d5/forecast/q/autoip.json',true)
 
 ourRequest.onload = function(){
+
 var weatherData = JSON.parse(ourRequest.responseText);
     //test
     var currentDayData = weatherData.forecast.simpleforecast.forecastday[0];
-  
 
+    console.log(weatherData);
     console.log(currentDayData);
     renderHTML(currentDayData);
 }
